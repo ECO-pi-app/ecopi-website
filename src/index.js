0e16df1e3d8e269e8471e900a28e2a06ab64e957
ECO-pi-app <ecopi.help@gmail.com>
2026-03-10 22:17:02 +0800

    yt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'handsontable/dist/handsontable.full.min.css'; // first
+import 'handsontable/css/handsontable.min.css';
 import './components/pages/ExcelEditor.css';          // overrides last
 
 
